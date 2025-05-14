da99c36e675b70d181b62d4d067cb7ad2062c26f
Oliviero Taleghani <o.taleghani@talesign.com>
2025-05-14 16:45:11 +0200

    fix: added navigation

--- src/components/modules/header.tsx
+++ src/components/modules/header.tsx
@@ -1,41 +1,182 @@
+import { cn } from "@/lib/utils";
+import {
+  forwardRef,
+  type ElementRef,
+  type ComponentPropsWithoutRef,
+} from "react";
 import {
   NavigationMenu,
-  NavigationMenuContent,
   NavigationMenuItem,
   NavigationMenuLink,
   NavigationMenuList,
-  NavigationMenuTrigger,
-  navigationMenuTriggerStyle,
 } from "@/components/ui/navigation-menu";
+import { useIsMobile } from "@/hooks/useIsMobile";
+import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
+import { AlignRight, ChevronDown, Hamburger } from "lucide-react";
+import { Button } from "../ui/button";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "../ui/sheet";
+
+const navItems = [
+  {
+    title: "Servizi",
+    subItems: [
+      {
+        title: "Per i proprietari",
+        href: "/servizi-proprietari",
+        description: "Lorem ipsum sit dolor et amet",
+      },
+      {
+        title: "Per gli ospiti",
+        href: "/servizi-ospiti",
+        description: "Lorem ipsum sit dolor et amet",
+      },
+    ],
+  },
+  {
+    title: "Chi siamo",
+    href: "/chi-siamo",
+  },
+  {
+    title: "Appartamenti",
+    href: "/appartamenti",
+  },
+  {
+    title: "Contatti",
+    href: "/contatti",
+  },
+];
 
 export default function Header() {
-  return (
-    <div>
-      <NavigationMenu>
-        <NavigationMenuList>
-          <NavigationMenuItem>
-            <NavigationMenuTrigger>Servizi</NavigationMenuTrigger>
-            <NavigationMenuContent></NavigationMenuContent>
-          </NavigationMenuItem>
+  const isMobile = useIsMobile();
 
-          <NavigationMenuItem>
-            <NavigationMenuLink href="/chi-siamo">Chi siamo</NavigationMenuLink>
-          </NavigationMenuItem>
-          <NavigationMenuItem>
-            <NavigationMenuLink href="/appartamenti">
-              Appartamenti
-            </NavigationMenuLink>
-          </NavigationMenuItem>
-          <NavigationMenuItem>
-            <NavigationMenuLink href="/contatti">Contatti</NavigationMenuLink>
-          </NavigationMenuItem>
-          <NavigationMenuItem>
-            <NavigationMenuLink href="/consulenza">
-              Richiedi una consulenza
-            </NavigationMenuLink>
-          </NavigationMenuItem>
-        </NavigationMenuList>
-      </NavigationMenu>
-    </div>
+  if (!isMobile) {
+    return (
+      <header className="sticky top-0 z-50 w-full bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+        <div className="container flex h-14 items-center justify-between">
+          <a href="/" className="mr-6 flex items-center space-x-2">
+            <span className="font-bold text-lg">Laif Apartments</span>
+          </a>
+          <div className="grow">
+            <NavigationMenu className="font-bold" orientation="horizontal">
+              <NavigationMenuList>
+                {navItems.map((item) =>
+                  item.subItems ? (
+                    <Popover key={item.title}>
+                      <PopoverTrigger className="flex gap-2 items-center text-sm">
+                        {item.title}
+                        <ChevronDown className="h-4 w-4" />
+                      </PopoverTrigger>
+                      <PopoverContent>
+                        {item.subItems.map((subItem) => (
+                          <ListItem
+                            key={subItem.title}
+                            href={subItem.href}
+                            title={subItem.title}
+                          >
+                            {subItem.description}
+                          </ListItem>
+                        ))}
+                      </PopoverContent>
+                    </Popover>
+                  ) : (
+                    <NavigationMenuItem key={item.title}>
+                      <NavigationMenuLink href={item.href}>
+                        {item.title}
+                      </NavigationMenuLink>
+                    </NavigationMenuItem>
+                  ),
+                )}
+              </NavigationMenuList>
+            </NavigationMenu>
+          </div>
+          <div className="">
+            <Button asChild className="font-bold">
+              <a href="/consulenza">Richiedi una consulenza</a>
+            </Button>
+          </div>
+        </div>
+      </header>
+    );
+  }
+
+  return (
+    <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+      <div className="container flex h-14 items-center justify-between">
+        <a href="/" className="mr-6 flex items-center space-x-2">
+          <span className="font-bold text-lg">Laif Apartments</span>
+        </a>
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button variant="secondary">
+              <AlignRight />
+            </Button>
+          </SheetTrigger>
+          <SheetContent className="w-[800px] sm:max-w-[80vw]">
+            <SheetHeader className="bg-accent">
+              <SheetTitle>Laif Apartments</SheetTitle>
+              <SheetDescription>Lorem ipsum sit dolor et amet</SheetDescription>
+            </SheetHeader>
+            <nav>
+              {navItems.map((item) =>
+                item.subItems ? (
+                  <div key={item.title}>
+                    {item.subItems.map((subItem) => (
+                      <div className="block px-4 py-3 text-muted-foreground hover:text-foreground font-bold text-sm">
+                        <a key={subItem.title} href={subItem.href}>
+                          {subItem.title}
+                        </a>
+                      </div>
+                    ))}
+                  </div>
+                ) : (
+                  <div
+                    key={item.title}
+                    className="block px-4 py-3 text-muted-foreground hover:text-foreground font-bold text-sm"
+                  >
+                    <a href={item.href}>{item.title}</a>
+                  </div>
+                ),
+              )}
+
+              <div className="block px-4 py-3 ">
+                <Button asChild className="font-bold">
+                  <a href="/consulenza">Richiedi una consulenza</a>
+                </Button>
+              </div>
+            </nav>
+          </SheetContent>
+        </Sheet>
+      </div>
+    </header>
   );
 }
+
+const ListItem = forwardRef<ElementRef<"a">, ComponentPropsWithoutRef<"a">>(
+  ({ className, title, children, ...props }, ref) => {
+    return (
+      <NavigationMenuLink asChild>
+        <a
+          ref={ref}
+          className={cn(
+            "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
+            className,
+          )}
+          {...props}
+        >
+          <div className="text-sm font-bold leading-none">{title}</div>
+          <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
+            {children}
+          </p>
+        </a>
+      </NavigationMenuLink>
+    );
+  },
+);
+ListItem.displayName = "ListItem";
---
+++ src/components/modules/hero-home.tsx
@@ -0,0 +1,11 @@
+export default function HeroHome() {
+  return (
+    <section>
+      <img
+        className="mx-auto max-w-6xl -mt-7"
+        src="https://cdn.prod.website-files.com/65e5e5474c606fdc3f19a597/65e786e6fbfbdee6e30e5af1_discover-your-dream-home-now-image-property-x-webflow-template.jpg"
+      />
+      <div className="container"></div>
+    </section>
+  );
+}
